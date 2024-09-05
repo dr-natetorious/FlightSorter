ee0a14eb4abfe92dd0b2b2eb4b81747937642b39
Nate Bachmeier <N.Bachmeier8343@o365.ncu.edu>
2024-09-05 13:19:43 -0400

    server starts

---
+++ tests/server.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+});
+
+describe('GET /', () => {
+    it('should return Hello World', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toBe('Hello World!');
+    });
+});
