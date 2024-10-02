2ce3b13b5d569736704f4ab8a9183584f6fbdd1e
Nate Bachmeier <dr@nate-explains.ai>
2024-10-02 16:19:47 -0400

    grammar

--- swagger.ts
+++ swagger.ts
@@ -6,7 +6,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'VolumeFI Takehome Test',
+      title: 'FlightSorter Takehome Test',
       version: '1.0.0',
       description: 'API documentation for my Express application',
     },
